60ca8a4e9e7efa2b0bc27f886150743e8428e73c
catalsdevelop <catalsdevelop@gmail.com>
2016-11-22 15:47:00 +0800

    增加了ftp模块connect get mkdir三个函数的单元测试代码

--- test/ftpTest.js
+++ test/ftpTest.js
@@ -4,6 +4,7 @@ const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const path = require('path');
+const fs = require('fs');
 const Base = require('../src/baseUploader');
 const utils = require('./utils');
 const base = require('jdf-file').base;
@@ -14,7 +15,40 @@ chai.use(sinonChai);
 
 describe('src/ftp', () => {
   const config = utils.defaultConfig('ftp');
-  const uploader = Base.create('ftp', config);
+  let uploader = Base.create('ftp', config);
+  let applyed = false;
+  // fake 几个需要的函数
+  let connectFake, mkdirFake, putFake;
+
+  beforeEach(()=> {
+    connectFake = sinon.stub(uploader.client, 'connect', function () {
+      if (!applyed) {
+        uploader.client.emit('ready');
+        applyed = true;
+      }
+      uploader.client.connected = true;
+    });
+
+    mkdirFake = sinon.stub(uploader.client, 'mkdir', function (source, force, callback) {
+      if(source.indexOf('unknown') > -1) {
+        callback(new Error('unknown source'));
+      }
+      callback(null);
+    });
+
+    putFake = sinon.stub(uploader.client, 'put', function (source, target, callback) {
+      // console.log(source);
+      callback(null);
+    });
+  });
+
+  afterEach(() => {
+    connectFake.restore();
+    mkdirFake.restore();
+    putFake.restore();
+    uploader.client.connected = false;
+    applyed = false;
+  });
 
   describe('getFiles()', () => {
     it('返回某个目录下要上传的文件信息', () => {
@@ -40,37 +74,6 @@ describe('src/ftp', () => {
   });
 
   describe('upload()', () => {
-    let applyed = false;
-    // fake 几个需要的函数
-    let connectFake, mkdirFake, putFake;
-
-    beforeEach(()=> {
-      connectFake = sinon.stub(uploader.client, 'connect', function () {
-        if (!applyed) {
-          uploader.client.emit('ready');
-          applyed = true;
-        }
-        uploader.client.connected = true;
-      });
-
-      mkdirFake = sinon.stub(uploader.client, 'mkdir', function (source, force, callback) {
-        // console.log(source);
-        callback(null);
-      });
-
-      putFake = sinon.stub(uploader.client, 'put', function (source, target, callback) {
-        // console.log(source);
-        callback(null);
-      });
-    });
-
-    afterEach(() => {
-      connectFake.restore();
-      mkdirFake.restore();
-      putFake.restore();
-      uploader.client.connected = false;
-      applyed = false;
-    });
 
     describe('上传文件夹', () => {
       const files = [
@@ -155,4 +158,123 @@ describe('src/ftp', () => {
       });
     })
   });
+
+  describe('connect()', () => {
+
+    beforeEach(() => {
+      connectFake.restore();
+      connectFake = sinon.stub(uploader.client, 'connect', function (options) {
+        if (options.host === 'unknown') {
+          uploader.client.emit('error', new Error('unknown host'));
+          return;
+        }
+        if (!applyed) {
+          uploader.client.emit('ready');
+          applyed = true;
+        }
+        uploader.client.connected = true;
+      });
+    });
+
+    it('连接错误，触发error', () => {
+      uploader.options.host = 'unknown';
+      const resolveCall = sinon.stub();
+      const rejectCall = sinon.stub();
+      return uploader.connect()
+        .then(resolveCall)
+        .catch(rejectCall)
+        .then(() => {
+          resolveCall.callCount.should.equal(0);
+          rejectCall.should.calledOnce;
+        });
+    });
+
+    it('连接正常，触发ready', () => {
+      uploader.options.host = utils.defaultConfig('ftp');
+      const resolveCall = sinon.stub();
+      const rejectCall = sinon.stub();
+      return uploader.connect()
+        .then(resolveCall)
+        .catch(rejectCall)
+        .then(() => {
+          resolveCall.should.calledOnce;
+          rejectCall.callCount.should.equal(0);
+        });
+    });
+
+    it('连接正常，多次连接', () => {
+      uploader.options.host = utils.defaultConfig('ftp');
+      const resolveCall = sinon.stub();
+      const rejectCall = sinon.stub();
+      return Promise.all([
+        uploader.connect().then(resolveCall).catch(rejectCall),
+        uploader.connect().then(resolveCall).catch(rejectCall)
+      ]).then(() => {
+          resolveCall.should.calledTwice;
+          rejectCall.callCount.should.equal(0);
+        });
+    })
+
+  });
+
+  describe('mkdir()', () => {
+    it('创建文件夹出错', () => {
+      const resolveCall = sinon.stub();
+      const rejectCall = sinon.stub();
+      return uploader.mkdir('c:\\unknown\\', 'page.jd.com/product/index/1.0.0/test/')
+        .then(resolveCall)
+        .catch(rejectCall)
+        .then(() => {
+          rejectCall.should.calledOnce;
+          resolveCall.callCount.should.equal(0);
+        });
+    })
+  });
+
+  describe('get()', () => {
+    let getFake;
+    const root = path.join(__dirname, './files/');
+    const tmpPath = path.resolve(root, 'config-tmp.json');
+    beforeEach(() => {
+      getFake = sinon.stub(uploader.client, 'get', (source, callback) => {
+        if(source.indexOf('fakeUri') > -1) {
+          callback(new Error('unknown source uri'));
+          return;
+        }
+        const stream = fs.createReadStream(path.resolve(root, 'config.json'));
+        callback(null, stream);
+      })
+    });
+
+    afterEach(() => {
+      getFake.restore();
+      if(fs.existsSync(tmpPath)) {
+        fs.unlinkSync(tmpPath);
+      }
+    });
+
+    it('正常获取，触发resolve', () => {
+      const resolveCall = sinon.stub();
+      const rejectCall = sinon.stub();
+      return uploader.get('hello.txt', path.resolve(__dirname, './files/config-tmp.json'))
+        .then(resolveCall)
+        .catch(rejectCall)
+        .then(() => {
+        resolveCall.should.calledOnce;
+        rejectCall.callCount.should.equal(0);
+      });
+    });
+
+    it('文件路径错误，触发reject', () => {
+      const resolveCall = sinon.stub();
+      const rejectCall = sinon.stub();
+      return uploader.get('page.jd.com/fakeUri', path.resolve(__dirname, './files/config-tmp.json'))
+        .then(resolveCall)
+        .catch(rejectCall)
+        .then(() => {
+          resolveCall.callCount.should.equal(0);
+          rejectCall.should.calledOnce;
+        });
+    })
+  })
 });
